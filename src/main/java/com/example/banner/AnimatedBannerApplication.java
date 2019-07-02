d60fbbf2b9040df4d176899fdce681e31e7b67ef
Jigyoomin <earth1223@sk.com>
2019-07-02 09:41:25 +0900

    Just print banner

--- src/main/java/com/example/banner/AnimatedBannerApplication.java
+++ src/main/java/com/example/banner/AnimatedBannerApplication.java
@@ -6,7 +6,4 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AnimatedBannerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AnimatedBannerApplication.class, args);
-	}
 }
---
+++ src/main/java/com/example/banner/Test.java
@@ -0,0 +1,35 @@
+package com.example.banner;
+
+import org.springframework.boot.ImageBanner;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
+import org.springframework.core.env.StandardEnvironment;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.ClassUtils;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int count = 5;
+        
+        if (args != null && args.length > 0 && args[0] != null) {
+            count = Integer.parseInt(args[0]);
+        }
+        
+        ClassLoader defaultClassLoader = ClassUtils.getDefaultClassLoader();
+        ResourceLoader resourceLoader = new DefaultResourceLoader(defaultClassLoader);
+        Resource resource = resourceLoader.getResource("classpath:banner.gif");
+        StandardEnvironment environment = new StandardEnvironment();
+//        MutablePropertySources sources = environment.getPropertySources();
+//        sources.addFirst(new SimpleCommandLinePropertySource(args));
+        
+        ImageBanner imageBanner = new ImageBanner(resource);
+        
+        for (int i = 0 ; i < count ; i++) {
+            imageBanner.printBanner(environment, Test.class, System.out);
+            System.out.print("\033[41A\r");
+        }
+    }
+}
